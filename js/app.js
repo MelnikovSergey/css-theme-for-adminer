938ccf163c2e22bf9f666bf4b76ee2610d3ce059
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2019-03-10 02:26:48 +0300

    Draw book on the screen

--- js/app.js
+++ js/app.js
@@ -14,5 +14,32 @@ function addBookToLibrary () {
 	}
 
 	books[randomArticle] = transformArray;
-	console.log(books);
+
+	drawBook(randomArticle);
+}
+
+// View
+function drawBook(article) {
+	
+	var div = document.createElement('div');
+	div.className = "col-lg-3 book";
+	div.setAttribute('data', article);
+
+	var cover = document.createElement('div');
+	cover.className = "book-cover";
+	cover.style.backgroundImage = `url(${books[article]['book-cover']})`;
+
+	var bookName = document.createElement('h4');
+	bookName.className = "book-title";
+	bookName.innerHTML = books[article]['book-name'];
+
+	var bookYear = document.createElement('p');
+	bookYear.className = "book-year";
+	bookYear.innerHTML = books[article]['book-year'];
+
+	div.appendChild(cover);
+	div.appendChild(bookName);
+	div.appendChild(bookYear);
+
+	$('.book-panel').append(div);
 }
